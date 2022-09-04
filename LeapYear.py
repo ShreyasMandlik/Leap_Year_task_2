3506ef4456665fe8f0731d3c59bf60593d402216
Shreyas <mandlikshreyas01@gmail.com>
2022-09-04 20:43:50 +0530

    Initial commit

---
+++ LeapYear.py
@@ -0,0 +1,4 @@
+
+
+def LeapYear(Year):
+    pass
---
+++ test_leapYear.py
@@ -0,0 +1,9 @@
+import unittest
+from unittest import result
+import LeapYear
+
+
+class TestLeapYear(unittest.TestCase):
+    
+    def test_leapYear(self):
+        pass
